2f08df8481bc1c74dca038d749a63efcd098b043
study-hary-id <haryansyah.study.id@gmail.com>
2021-02-22 14:33:54 +0700

    Add 'gorilla/mux' framework Create new router and 5 handlers

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	var r = mux.NewRouter()
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBooks).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
